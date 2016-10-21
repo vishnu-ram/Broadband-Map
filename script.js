b477525aab4f03ffb3e0122180965007f23b2337
Kexin Zhang <kxin.zhang@gmail.com>
2016-10-21 02:09:31 -0400

    added comments for js tasks

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ var map = kartograph.map('#map');
 
 map.loadMap('all_simplified.svg', function() {
 	map.addLayer('countylayer');
+
+	//TODO
+
+	//add color
+	//tooltip on hover
+	//legend
+	//zoom, click and drag, pan, etc
+	//animations on hover (change opacity, outline county, stuff like that)
+	//search functionality
+
 });
